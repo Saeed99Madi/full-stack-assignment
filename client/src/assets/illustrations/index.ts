7db9bdea5290d82ff250a1d040744237de8d6978
Saeed99Madi <saed.zooro@gmail.com>
2024-12-18 15:28:53 +0200

    add missing files for the error pages related to #3

---
+++ client/src/assets/illustrations/index.ts
@@ -0,0 +1,5 @@
+
+export { default as PageNotFoundIllustration } from './page-not-found-illustration';
+export { default as ForbiddenIllustration } from './forbidden-illustration';
+export { default as BackgroundShape } from './background-shape';
+export { default as SeverErrorIllustration } from './sever-error-illustration';

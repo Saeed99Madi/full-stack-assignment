e48b1b9cf8f45bd17765d62fce47d4738184eda4
Saeed99Madi <saed.zooro@gmail.com>
2024-12-18 13:56:42 +0200

    full Set up Skeleton For Front End related to #2

---
+++ client/src/_mock/index.ts
@@ -0,0 +1,8 @@
+export * from './assets';
+export * from './_mock';
+
+// ----------------------------------------------------------------------
+
+
+export * from './_product';
+export * from './_user';

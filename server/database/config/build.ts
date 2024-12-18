f039ebff3aaaec73e511e77bf239b6d7979cc074
Saeed99Madi <saed.zooro@gmail.com>
2024-12-18 15:31:50 +0200

    set up the data base cofigrations and edit the database settings to work in deferant envs related to #3

---
+++ server/database/config/build.ts
@@ -0,0 +1,19 @@
+import { sequelize } from '.';
+import { BUILD_DB } from '../../config';
+import insertData from './insertData';
+
+const build = async () => {
+  try {
+    await sequelize.sync({ force: true });
+    await insertData();
+    console.log('Database build complete');
+  } catch (error) {
+    console.error('Error building database:', error);
+  }
+};
+
+if (BUILD_DB) {
+  build();
+}
+
+export default build;
---
+++ server/database/config/insertData.ts
@@ -0,0 +1,8 @@
+import data from './seeds.json';
+
+const insertData = async (): Promise<void> => {
+  // Seed the Data
+  // await User.bulkCreate(data.Users);
+};
+
+export default insertData;

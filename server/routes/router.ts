fbe57b5590843d499c47e67e456ad2eece7649fa
Saeed99Madi <saed.zooro@gmail.com>
2024-12-19 13:36:03 +0200

    create the product router file related to #6

--- server/routes/router.ts
+++ server/routes/router.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { errorWrapper } from '../utils';
 import { authRouter } from './auth';
+import { productRouter } from './products';
 
 const router: Router = Router();
 router.use('/user', errorWrapper(authRouter));
+router.use('/product', errorWrapper(productRouter));
 
 export { router };

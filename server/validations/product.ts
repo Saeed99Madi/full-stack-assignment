bbd4868c3e2548bc2e7660e3f19069376fc7f25c
Saeed99Madi <saed.zooro@gmail.com>
2024-12-20 18:02:40 +0200

    edit the adding fucntionality based on the last edit in the product model schema related to #26

--- server/validations/product.ts
+++ server/validations/product.ts
@@ -2,9 +2,22 @@ import * as Joi from 'joi';
 
 const productSchema = (data: object) => {
   const schema = Joi.object({
-    title: Joi.string().min(4).max(50).required(),
-    description: Joi.string().min(50).max(5000).required(),
-    CategoryId: Joi.string().required(),
+    name: Joi.string().min(4).max(50).required(),
+    gender: Joi.string().min(4).max(50).required(),
+    publish: Joi.string().min(4).max(50).required(),
+    category: Joi.string().min(4).max(50).required(),
+    inventoryType: Joi.string().min(4).max(50).required(),
+    description: Joi.string().min(4).max(5000).required(),
+    subDescription: Joi.string().min(4).max(5000).required(),
+    coverUrl: Joi.string().min(4).max(50).required(),
+    price: Joi.number().min(4).max(50).required(),
+    priceSale: Joi.number().min(4).max(50).required(),
+    totalRatings: Joi.number().min(4).max(50).required(),
+    totalSold: Joi.number().min(4).max(50).required(),
+    totalReviews: Joi.number().min(4).max(50).required(),
+    taxes: Joi.number().min(4).max(50).required(),
+    quantity: Joi.number().min(4).max(50).required(),
+    available: Joi.string().min(4).max(50).required(),
   });
   return schema.validateAsync(data);
 };
